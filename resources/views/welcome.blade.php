08f842fff7af653697af78389658c882856f49ef
CandelasHernandezJossueAlejandro <al222110811@gmail.com>
2024-01-31 13:33:18 -0600

    Inclusión de layout y estilos de la página. Unicamente del dashboard

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,12 +1,57 @@
-<h1>Inicio</h1>
+@extends('layout.layout')
 
-@auth
-    <p>Bienvenido {{ Auth::user()->name.' '.Auth::user()->app.' '.Auth::user()->apm }} estas autenticado!</p>
-    <p>
-        <a href="/logout">Cerrar Sesión</a>
-    </p>
-@endauth
+<!-- Contenido de la página START -->
+@section('content')
+<div class="col-md-12">
+    <div class="card ">
+        <div class="card-header ">
+            <h5 class="card-title px-3 pt-2">Usuarios</h5>
+            <p class="card-category px-3">Lista de usuarios que han hecho uso de la biblioteca.</p>
+        </div>
+        <div class="card-body ">
+            <table class="table table-bordered">
+                <thead>
+                    <tr>
+                        <th scope="col">#</th>
+                        <th scope="col">First</th>
+                        <th scope="col">Last</th>
+                        <th scope="col">Handle</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr>
+                        <th scope="row">1</th>
+                        <td>Mark</td>
+                        <td>Otto</td>
+                        <td>@mdo</td>
+                    </tr>
+                    <tr>
+                        <th scope="row">2</th>
+                        <td>Jacob</td>
+                        <td>Thornton</td>
+                        <td>@fat</td>
+                    </tr>
+                    <tr>
+                        <th scope="row">3</th>
+                        <td colspan="2">Larry the Bird</td>
+                        <td>@twitter</td>
+                    </tr>
+                </tbody>
+            </table>
+        </div>
+    </div>
+</div>
+@endsection
+<!-- Contenido de la página END -->
 
-@guest
-    <p>Para ver el contenido Inicia Sesión</p>
-@endguest
+@section('js')
+<script>
+    // Selector de la sección en el navbar
+    var navbar = document.querySelector('#inicio');
+    navbar.className = "active";
+
+    // Titulo de la paginá
+    var titulo = document.querySelector('#titulo');
+    titulo.innerHTML = 'Inicio';
+</script>
+@endsection
