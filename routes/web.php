3e7d2daa79ad92b85e6ce0586d0514cac1366fd2
CandelasHernandezJossueAlejandro <al222110811@gmail.com>
2024-03-11 16:25:10 -0600

    Restauración rutas de reportes

--- routes/web.php
+++ routes/web.php
@@ -31,7 +31,6 @@ Route::get('/', function () {
     }
 })->name('/');
 
-
 //  Login
 Route::get('/login', [AuthController::class, 'show'])->name('login');
 Route::post('/login', [AuthController::class, 'login']);
@@ -41,46 +40,30 @@ Route::get('/logout', [LogoutController::class, 'logout']);
 Route::middleware('auth')->group(function () {
     Route::get('/', [RentaMaquinasController::class, 'index'])->name('inicio');
 
-
-
-
-
-    // Route::get('/', function () {
-    //     $maquinas = RentaMaquinasController::index();
-    //     $libros = RentaLibroController::index();
-    //     return view('tu_vista', compact('maquinas', 'libros'));
-    // });
-
     //  Resourse usuarios
     Route::resource('usuarios', UsuariosController::class);
-    Route::get('usuarios', [UsuariosController::class, 'index'])->name('usuarios.index');
-    Route::POST('usuarios/store', [UsuariosController::class, 'store'])->name('usuarios.store');
-    Route::delete('usuarios/delete/{id}', [UsuariosController::class, 'destroy'])->name('usuarios.destroy');
+    Route::get('usuarios', [UsuariosController::class, 'index'])->name('usuarios.index')->middleware('role:1');
+    Route::POST('usuarios/store', [UsuariosController::class, 'store'])->name('usuarios.store')->middleware('role:1');
+    Route::delete('usuarios/delete/{id}', [UsuariosController::class, 'destroy'])->name('usuarios.destroy')->middleware('role:1');
 
     // Resourse Libros
-    Route::resource('libros', LibrosController::class);
-    Route::get('libros', [LibrosController::class, 'index'])->name('libros.index');
-    Route::POST('libros/store', [LibrosController::class, 'store'])->name('libros.store');
-    Route::delete('libros/delete/{id}', [LibrosController::class, 'destroy'])->name('libros.destroy');
-    Route::name('reporteslibros')->get('reporteslibros',[LibrosController::class, 'reporteslibros']);
-
+    Route::resource('libros', LibrosController::class)->middleware('role:1');
+    Route::get('libros', [LibrosController::class, 'index'])->name('libros.index')->middleware('role:1');
+    Route::POST('libros/store', [LibrosController::class, 'store'])->name('libros.store')->middleware('role:1');
+    Route::delete('libros/delete/{id}', [LibrosController::class, 'destroy'])->name('libros.destroy')->middleware('role:1');
 
     //  Resource Maquinas
-    Route::resource('maquinas', MaquinasController::class);
-    Route::put('/maquinas/{id}', 'MaquinasController@update')->name('maquinas.update');
-    Route::DELETE('/maquinas/{id}', 'MaquinasController@destroy')->name('maquinas.destroy');
+    Route::resource('maquinas', MaquinasController::class)->middleware('role:1');
+    Route::put('/maquinas/{id}', 'MaquinasController@update')->name('maquinas.update')->middleware('role:1');
+    Route::DELETE('/maquinas/{id}', 'MaquinasController@destroy')->name('maquinas.destroy')->middleware('role:1');
     Route::name('reportes')->get('reportes',[MaquinasController::class, 'reportes']);
-
-
 
     //  Renta de maquinas
     Route::name('rentarmaquina')->post('rentarmaquina', [RentaMaquinasController::class, 'rentarmaquina']);
     Route::put('rentaMaquina.update/{id}', [RentaMaquinasController::class, 'update'])->name('rentaMaquina.update');
+    
     //  RUTAS de renta LIBROS
     Route::resource('renta-libros', RentaLibroController::class);
     Route::get('/mostrar-aviso-devolucion', [RentaMaquinasController::class, 'mostrarAvisoDevolucion'])->name('mostrar.aviso.devolucion');
-    
-
-
 
 });
