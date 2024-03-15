1b103b0051b842e6c93cd59926f74e07a8814977
MIKE <al222010230@gmail.com>
2024-03-15 11:37:11 -0600

    SELECT DINAMICO(SEDDEERS) FULL

--- database/seeders/UsersSeeder.php
+++ database/seeders/UsersSeeder.php
@@ -58,72 +58,64 @@ class UsersSeeder extends Seeder
                 $x++; // Incrementamos $x aquí
             }
         }
+        // Insertar datos de prueba en la tabla 'users'
 
+        //intyeccion
+        DB::table('users')->insert([
+            'nombre' => 'Administrador',
+            'app' => '',
+            'apm' => '',
+            'carrera' => 'UTVT',
+            'matricula' => 123456,
+            'direccion' => 'Lerma México',
+            'genero' => 'M',
+            'email' => 'admin@biblioteca.com',
+            'password' => Hash::make('admin'),
+            'rol_id' => 1,
+            'created_at' => now(),
+            'updated_at' => now(),
+        ]);
 
+        DB::table('users')->insert([
+            'nombre' => 'Auxiliar',
+            'app' => '-',
+            'apm' => 'Biblioteca',
+            'genero' => 'M',
+            'email' => 'auxiliar@biblioteca.com',
+            'password' => Hash::make('auxiliar'),
+            'rol_id' => 2,
+            'created_at' => now(),
+            'updated_at' => now(),
+        ]);
+
+        DB::table('users')->insert([
+            'nombre' => 'Miguel Emanuel',
+            'app' => 'Arriola',
+            'apm' => 'Ortega',
+            'carrera' => 'UTVT',
+            'matricula' => 123456,
+            'direccion' => 'Xonacatlan, Edo. de México',
+            'genero' => 'H',
+            'email' => 'mike@biblioteca.com',
+            'password' => Hash::make('mike'),
+            'rol_id' => 3,
+            'created_at' => now(),
+            'updated_at' => now(),
+        ]);
+
+        DB::table('users')->insert([
+            'nombre' => 'Jimena',
+            'app' => 'Diaz',
+            'apm' => 'de los Santos',
+            'carrera' => 'UTVT',
+            'matricula' => 123456,
+            'direccion' => 'Xonacatlan, Edo. de México',
+            'genero' => 'H',
+            'email' => 'jime@biblioteca.com',
+            'password' => Hash::make('jime'),
+            'rol_id' => 3,
+            'created_at' => now(),
+            'updated_at' => now(),
+        ]);
     }
 }
-
-// class UsersSeeder extends Seeder
-// {
-//     /**
-//      * Run the database seeds.
-//      */
-//     public function run(): void
-//     {
-//         // Insertar datos de prueba en la tabla 'users'
-
-//         //intyeccion
-//         DB::table('users')->insert([
-//             'nombre' => 'Administrador',
-//             'app' => '',
-//             'apm' => '',
-//             'carrera' => 'UTVT',
-//             'matricula' => 123456,
-//             'direccion' => 'Lerma México',
-//             'genero' => 'M',
-//             'email' => 'admin@biblioteca.com',
-//             'password' => Hash::make('admin'),
-//             'rol_id' => 1,
-//             'created_at' => now(),
-//             'updated_at' => now(),
-//         ]);
-
-//         DB::table('users')->insert([
-//             'nombre' => 'Auxiliar',
-//             'app' => '-',
-//             'apm' => 'Biblioteca',
-//             'genero' => 'M',
-//             'email' => 'auxiliar@biblioteca.com',
-//             'password' => Hash::make('auxiliar'),
-//             'rol_id' => 2,
-//             'created_at' => now(),
-//             'updated_at' => now(),
-//         ]);
-
-//         DB::table('users')->insert([
-//             'nombre' => 'Miguel Emanuel',
-//             'app' => 'Arriola',
-//             'apm' => 'Ortega',
-//             'carrera' => 'UTVT',
-//             'matricula' => 123456,
-//             'direccion' => 'Xonacatlan, Edo. de México',
-//             'genero' => 'H',
-//             'rol_id' => 3,
-//             'created_at' => now(),
-//             'updated_at' => now(),
-//         ]);
-
-//         DB::table('users')->insert([
-//             'nombre' => 'Jimena',
-//             'app' => 'Diaz',
-//             'apm' => 'de los Santos',
-//             'carrera' => 'UTVT',
-//             'matricula' => 123456,
-//             'direccion' => 'Xonacatlan, Edo. de México',
-//             'genero' => 'H',
-//             'rol_id' => 3,
-//             'created_at' => now(),
-//             'updated_at' => now(),
-//         ]);
-//     }
-// }
