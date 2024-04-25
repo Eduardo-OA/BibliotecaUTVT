f3ada62dff8eba841663c31c46ac87b7ea3fe449
LopezalarconJuanCarlos <al222220002@gmail.com>
2024-04-25 07:35:16 -0600

    graficas y excel

--- config/app.php
+++ config/app.php
@@ -156,13 +156,15 @@ return [
     */
 
     'providers' => ServiceProvider::defaultProviders()->merge([
+        
         /*
          * Package Service Providers...
          */
 
         /*
          * Application Service Providers...
-         */
+         */  
+        
         App\Providers\AppServiceProvider::class,
         App\Providers\AuthServiceProvider::class,
         // App\Providers\BroadcastServiceProvider::class,
@@ -183,6 +185,7 @@ return [
 
     'aliases' => Facade::defaultAliases()->merge([
         // 'Example' => App\Facades\Example::class,
+        
     ])->toArray(),
 
 ];
